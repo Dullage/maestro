df701d1ff345aa35ad18762baaa802d7827a028a
Adam Dullage <adam@dullage.com>
2020-10-20 13:33:52 +0100

    Config validation, refactor and comment

--- maestro/maestro.py
+++ maestro/maestro.py
@@ -1,15 +1,46 @@
+import json
 from json import JSONDecodeError
 
 import paho.mqtt.client as mqtt
 import yaml
-from marshmallow import Schema, ValidationError, fields
-
 from animations import animations
 from light import Light
 from logger import log
+from marshmallow import Schema, ValidationError, fields, validate
+
+
+class ConfigSchemaMQTT(Schema):
+    """Schema for the MQTT section of the YAML config file."""
+
+    host = fields.String(required=True)
+    port = fields.Int(validate=validate.Range(min=0, max=65535), missing=1883)
+    base_topic = fields.String(missing="maestro")
+    client_id = fields.String(missing="maestro")
+
+
+class ConfigSchemaLight(Schema):
+    """Schema for the light section of the YAML config file."""
+
+    host = fields.String(required=True)
+    port = fields.Int(validate=validate.Range(min=0, max=65535), missing=21324)
+    num_leds = fields.Int(validate=validate.Range(min=1), required=True)
+    animation_fps = fields.Int(missing=30)
+
+
+class ConfigSchema(Schema):
+    """Main schema for the YAML config file."""
+
+    mqtt = fields.Nested(ConfigSchemaMQTT)
+    lights = fields.Dict(
+        keys=fields.String(),
+        values=fields.Nested(ConfigSchemaLight),
+        validate=validate.Length(min=1),
+    )
 
 
 class AnimationStartSchema(Schema):
+    """Schema for the JSON MQTT payload to start an animation."""
+
     animation = fields.Str(required=True)
     config = fields.Dict(missing={})
 
@@ -23,89 +54,93 @@ class Maestro:
     animation_start_schema = AnimationStartSchema()
 
     def __init__(self):
-        self._config = self._load_config()
+        # Load Config
+        self.config = self.load_config()
 
-        self._lights = {}
-        for name, config in self._config["lights"].items():
+        # Initialise Lights
+        self.lights = {}
+        for name, config in self.config["lights"].items():
             host = config["host"]
             port = config["port"]
             num_leds = config["num_leds"]
             animation_fps = config["animation_fps"]
 
-            self._lights[name] = Light(host, port, num_leds, animation_fps)
+            self.lights[name] = Light(host, port, num_leds, animation_fps)
 
             log.info(
-                f"Loaded light '{name}' ({num_leds} LEDs at {host}:{port})"
+                f"Initialised light '{name}' ({num_leds} LEDs at {host}:{port})"  # noqa
             )
 
-        for animation_name in animations.keys():
-            log.info(f"Loaded animation '{animation_name}'")
+        # Create MQTT Client
+        self.mqtt_client = mqtt.Client(self.config["mqtt"]["client_id"])
+        self.mqtt_client.on_connect = self.mqtt_on_connect
+        self.mqtt_client.on_message = self.mqtt_on_message
 
-        self._mqtt_client = None
-
-    def _load_config(self):
+    def load_config(self):
+        """Load and validate YAML config file."""
         with open("config.yaml", "r") as f:
-            return yaml.safe_load(f)
+            config = yaml.safe_load(f)
+        schema = ConfigSchema()
+        return schema.load(config)
 
     def run(self):
-        self._mqtt_client = mqtt.Client(self._config["mqtt"]["client_id"])
-        self._mqtt_client.on_connect = self._mqtt_on_connect
-        self._mqtt_client.on_message = self._mqtt_on_message
-        self._mqtt_client.connect(
-            self._config["mqtt"]["host"], self._config["mqtt"]["port"]
+        """Connect to the MQTT server and loop forever."""
+        self.mqtt_client.connect(
+            self.config["mqtt"]["host"], self.config["mqtt"]["port"]
         )
-        self._mqtt_client.loop_forever()
+        self.mqtt_client.loop_forever()
 
-    def _topics_for_light_name(self, light_name):
+    def get_topics_for_light(self, light_name):
+        """Return a list of topics to subscribe to for a particular light
+        name.
+        """
         base_topic_for_light = "/".join(
-            [self._config["mqtt"]["base_topic"], light_name]
+            [self.config["mqtt"]["base_topic"], light_name]
         )
-        topics = []
 
+        topics = []
         topics.append("/".join([base_topic_for_light, self.ON_INSTRUCTION]))
         topics.append("/".join([base_topic_for_light, self.OFF_INSTRUCTION]))
-        topics.append(
-            "/".join(
-                [
-                    base_topic_for_light,
-                    self.ANIMATION_INSTRUCTION,
-                    self.ANIMATION_START,
-                ]
+        for sub_instruction in [self.ANIMATION_START, self.ANIMATION_STOP]:
+            topics.append(
+                "/".join(
+                    [
+                        base_topic_for_light,
+                        self.ANIMATION_INSTRUCTION,
+                        sub_instruction,
+                    ]
+                )
             )
-        )
-        topics.append(
-            "/".join(
-                [
-                    base_topic_for_light,
-                    self.ANIMATION_INSTRUCTION,
-                    self.ANIMATION_STOP,
-                ]
-            )
-        )
         return topics
 
-    def _mqtt_on_connect(self, client, userdata, flags, rc):
-        for light_name in self._lights.keys():
-            for topic in self._topics_for_light_name(light_name):
+    def mqtt_on_connect(self, client, userdata, flags, rc):
+        """Subscribe to relevant topics upon connection to MQTT server."""
+        for light_name in self.lights.keys():
+            for topic in self.get_topics_for_light(light_name):
                 log.info(f"Subscribed to '{topic}'")
                 client.subscribe(topic)
 
-    def _animation_finished_callback(self, data):
-        self._mqtt_client.publish(
+    def animation_finished_callback(self, data):
+        """Publish an MQTT message signalling that an animation has
+        finished.
+        """
+        self.mqtt_client.publish(
             "/".join(
                 [
-                    self._config["mqtt"]["base_topic"],
+                    self.config["mqtt"]["base_topic"],
+                    data["light_name"],
                     self.ANIMATION_INSTRUCTION,
                     "finished",
                 ]
             ),
-            payload=data["animation_name"],
+            payload=data,
         )
 
-    def _mqtt_on_message(self, client, userdata, msg):
+    def mqtt_on_message(self, client, userdata, msg):
+        """Parse MQTT messages and perform the specified action."""
         topic = msg.topic.split("/")
         light_name, instruction = topic[1], topic[2]
-        light = self._lights[light_name]
+        light = self.lights[light_name]
 
         # Instructions
         if instruction == self.ON_INSTRUCTION:
@@ -118,18 +153,19 @@ class Maestro:
 
         elif instruction == self.ANIMATION_INSTRUCTION:
             anim_instruction = topic[3]  # TODO: Check behaviour if blank
+            payload = msg.payload.decode("utf-8")
 
             # Start
-            if anim_instruction == "start":
+            if anim_instruction == self.ANIMATION_START:
                 try:
                     validated_payload = self.animation_start_schema.loads(
-                        msg.payload.decode("utf-8")
+                        payload
                     )
                 except ValidationError as e:
-                    print(e.messages)  # TODO
+                    log.error(e.messages)
                     return
                 except JSONDecodeError as e:
-                    print(e)  # TODO
+                    log.error(e)
                     return
 
                 animation_name = validated_payload["animation"]
@@ -142,15 +178,15 @@ class Maestro:
                     )
                     light.start_animation(
                         animation,
-                        callback=self._animation_finished_callback,
-                        callback_data={"animation_name": animation_name},
+                        callback=self.animation_finished_callback,
+                        callback_data=payload,
                     )
                 except KeyError:
                     log.error(f"Unknown animation '{animation_name}'")
                     return
 
             # Stop
-            elif anim_instruction == "stop":
+            elif anim_instruction == self.ANIMATION_STOP:
                 log.info(f"Stopping animation on '{light_name}'")
                 light.stop_animation()
 
